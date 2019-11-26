32352c80d6fe26b1d5868057f3be9b335aab477c
Brandon Braun <brandonbraun653@gmail.com>
2019-11-25 21:21:20 -0600

    Added some comments on where I might want to take this.

---
+++ uLog/ulog.cpp
@@ -0,0 +1,18 @@
+/********************************************************************************
+ *  File Name:
+ *    ulog.cpp
+ *
+ *  Description:
+ *    uLog implementation
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+/* Driver Includes */
+#include <uLog/ulog.hpp>
+
+
+namespace uLog
+{
+
+}
---
+++ uLog/ulog.hpp
@@ -0,0 +1,26 @@
+/********************************************************************************
+ *  File Name:
+ *    ulog.hpp
+ *
+ *  Description:
+ *    uLog attempts to be a fairly simple logger for embedded systems
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once
+#ifndef MICRO_LOGGER_HPP
+#define MICRO_LOGGER_HPP
+
+namespace uLog
+{
+  /**
+   *  1. Set log level
+   *  2. info, warn, debug, critical
+   *  3. Add sinks somehow to the backend.
+   *  4. Should I support formatting???
+   *
+   */
+}
+
+#endif  /* MICRO_LOGGER_HPP */
